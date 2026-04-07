73730650c0e81eb57bebf6534a932923af8f31d7
ClemensMueller1 <clemens.mueller@mandelherz.de>
2026-04-07 17:27:43 +0200

    fix include

--- esphome/components/smabluetooth_solar/SMA_Inverter.cpp
+++ esphome/components/smabluetooth_solar/SMA_Inverter.cpp
@@ -23,6 +23,7 @@ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
 */
 #include <Arduino.h>
+#include <string>
 #include "SMA_Inverter.h"
 #include "esphome/core/application.h"
 
